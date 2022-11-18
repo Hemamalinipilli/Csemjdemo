3da855b61e2049c2ec5a2b63c940571032f9358f
honey <honey@192.168.0.105>
2022-11-18 21:24:21 +0530

    first jenkins demo

---
+++ src/main/java/Mavenjenkins/csemjdemo/App.java
@@ -0,0 +1,13 @@
+package Mavenjenkins.csemjdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to jenkins world!!" );
+    }
+}
